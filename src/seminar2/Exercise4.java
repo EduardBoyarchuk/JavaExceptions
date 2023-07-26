39e7e0754e786d05cbfdcbd073c0823ceab3f146
EduardBoyarchuk <boyarchuk.eduard@list.ru>
2023-07-26 22:30:18 +0300

    seminar2/3

--- src/seminar2/Exercise4.java
+++ src/seminar2/Exercise4.java
@@ -1,2 +1,23 @@
-package seminar2;public class Exercise4 {
+package seminar2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise4 {
+
+    public static void emptyString(){
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Введите текст: ");
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+                throw new Exception("Пустые строки вводить нельзя!");
+            }
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
 }
